a3f28ce501e6e32c5ab220647f515f961cc893c9
Evgeniy Privalov <evgeniyprivalov94@gmail.com>
2022-01-22 11:43:05 +0300

    Major updates to API 5.6 - Skip updates of 5.4 and 5.5

--- tgsdk/_version.py
+++ tgsdk/_version.py
@@ -3,6 +3,6 @@
 
 # Copyright (c) 2015-2021 Evgeniy Privalov, https://linkedin.com/in/evgeniyprivalov/
 
-__version__ = "1.0.20"
+__version__ = "1.0.21"
 
-__tg_version__ = "5.3"
+__tg_version__ = "5.6"
